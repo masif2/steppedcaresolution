036302d9486909062071cc3cfe019be517d1400f
Muharrem <masif@egenienext.com>
2021-07-29 17:14:58 +0500

    Report route

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ReportsController extends Controller
 {
     public function index(){
-        return view("Reports.index");
+        return view("reports.index");
     }
     
 
